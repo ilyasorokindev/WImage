d6e6f554b37916620970d5d89535834afcebed1d
Ilya Sorokin <ilya.sorokin@sigma.software>
2020-11-11 18:03:07 +0200

    Updated

--- Package.swift
+++ Package.swift
@@ -27,9 +27,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "WImage",
-            dependencies: []),
+            dependencies: [],
+            path: "WImage/Classes"),
         .testTarget(
             name: "WImageTests",
-            dependencies: ["WImage"]),
+            dependencies: ["WImage"],
+            path: "WImageTests"),
     ]
 )
