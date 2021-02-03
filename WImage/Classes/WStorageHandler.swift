e83ddc5eee9d96b4de0318070c09532ed8d2c778
Ilya Sorokin <ilya.sorokin@sigma.software>
2021-02-03 22:02:11 +0200

    master > add memory cache

--- WImage/Classes/WStorageHandler.swift
+++ WImage/Classes/WStorageHandler.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 public protocol WStorageHandlerProtocol {
-
+    
     func getImage(url: URL) -> WPlatformImage?
     func saveImage(url: URL, image: WPlatformImage, imageData: Data)
     func removeImage(url: URL)
@@ -16,59 +16,68 @@ public protocol WStorageHandlerProtocol {
 }
 
 internal class WStorageHandler: WStorageHandlerProtocol {
-
+    
     private let fileManager = FileManager.default
+    private var imageCache = [URL: WPlatformImage]()
     private let documentDirectoryUrl: URL = {
-      do {
-        let fileManager = FileManager.default
-        let url = try fileManager.url(for: .cachesDirectory, in: .userDomainMask, appropriateFor: nil, create: false).appendingPathComponent(Constants.cacheName, isDirectory: true)
-        try fileManager.createDirectory(at: url, withIntermediateDirectories: true, attributes: nil)
-        return url
-      } catch {
-        fatalError("can not get documentDirectoryUrl")
-      }
+        do {
+            let fileManager = FileManager.default
+            let url = try fileManager.url(for: .cachesDirectory, in: .userDomainMask, appropriateFor: nil, create: false).appendingPathComponent(Constants.cacheName, isDirectory: true)
+            try fileManager.createDirectory(at: url, withIntermediateDirectories: true, attributes: nil)
+            return url
+        } catch {
+            fatalError("can not get documentDirectoryUrl")
+        }
     }()
-
+    
     func getImage(url: URL) -> WPlatformImage? {
         let fileUrl = self.makeFileUrl(url: url)
+        if let image = self.imageCache[fileUrl] {
+            return image
+        }
         if let data = try? Data(contentsOf: fileUrl) {
-            return WPlatformImage(data: data, scale: Constants.scale)
+            let image = WPlatformImage(data: data, scale: Constants.scale)
+            self.imageCache[fileUrl] = image
+            return image
         }
         return nil
     }
-
+    
     func saveImage(url: URL, image: WPlatformImage, imageData: Data) {
         let fileUrl = self.makeFileUrl(url: url)
+        self.imageCache[fileUrl] = image
         do {
-          try imageData.write(to: fileUrl)
+            try imageData.write(to: fileUrl)
         } catch {
-          assertionFailure("Can not save file \(fileUrl.absoluteString)")
+            assertionFailure("Can not save file \(fileUrl.absoluteString)")
         }
     }
-
+    
     func removeImage(url: URL) {
         let fileUrl = self.makeFileUrl(url: url)
-      try? FileManager.default.removeItem(at: fileUrl)
+        self.imageCache[fileUrl] = nil
+        try? self.fileManager.removeItem(at: fileUrl)
     }
-
+    
     func removeAllImages() {
         DispatchQueue.global().async {
-          do {
-            try self.fileManager.contentsOfDirectory(at: self.documentDirectoryUrl,
-                                                includingPropertiesForKeys: nil,
-                                                options: [])
-              .forEach({ url in
-                try self.fileManager.removeItem(at: url)
-              })
-          } catch {
-            assertionFailure(error.localizedDescription)
-          }
+            do {
+                self.imageCache.removeAll()
+                try self.fileManager.contentsOfDirectory(at: self.documentDirectoryUrl,
+                                                         includingPropertiesForKeys: nil,
+                                                         options: [])
+                    .forEach({ url in
+                        try self.fileManager.removeItem(at: url)
+                    })
+            } catch {
+                assertionFailure(error.localizedDescription)
+            }
         }
     }
-
+    
     private func makeFileUrl(url: URL) -> URL {
         let name = Data(url.absoluteString.utf8).base64EncodedString().replacingOccurrences(of: "/", with: "")
         return documentDirectoryUrl.appendingPathComponent(name, isDirectory: false)
-      }
-
+    }
+    
 }
